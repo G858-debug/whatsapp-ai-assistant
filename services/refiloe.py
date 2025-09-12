4c1d305c0c60900139f01dd87c29d69603812bc9
G858-debug <mkhwalo88@gmail.com>
2025-09-12 23:13:29 +0200

    Update refiloe.py

--- services/refiloe.py
+++ services/refiloe.py
@@ -158,7 +158,7 @@ class RefiloeService:
                     "type": "reply",
                     "reply": {
                         "id": "reg_explore",
-                        "title": "📚 Learn More about me"
+                        "title": "📚 Learn about me"
                     }
                 }
             ]
