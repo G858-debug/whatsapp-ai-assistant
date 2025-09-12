19b98298e4be6f126fe8aefdcd726f090dab2362
G858-debug <mkhwalo88@gmail.com>
2025-09-12 21:33:10 +0200

    Update whatsapp.py

--- services/whatsapp.py
+++ services/whatsapp.py
@@ -18,8 +18,8 @@ class WhatsAppService:
         self.sa_tz = pytz.timezone(config.TIMEZONE)
 
         # Store WhatsApp credentials as instance attributes
-       self.access_token = config.ACCESS_TOKEN
-       self.phone_number_id = config.PHONE_NUMBER_ID
+        self.access_token = config.ACCESS_TOKEN
+        self.phone_number_id = config.PHONE_NUMBER_ID
         
         # Track processed messages to prevent duplicates
         self.processed_messages = set()
